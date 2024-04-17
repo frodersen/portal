76b65530289d46592544555ab342761e057013c9
Jonas Mørkland <jonasmorkland@outlook.com>
2024-04-17 13:59:24 +0200

    refactor: removed redundant vue component 'UserProfile.vue, since it should not display anything to the user. The remaining logic was encapsulated into a single function and placed in the first vue component 'LoginForm.vue'.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,13 @@
 // router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
-import LoginForm from '@/components/LoginForm.vue';
-import UserProfile from '@/components/UserProfile.vue';
-
+import { createRouter, createWebHistory } from "vue-router";
+import LoginForm from "@/components/LoginForm.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Login',
+    path: "/",
+    name: "Login",
     component: LoginForm,
   },
-  {
-    path: '/user-profile',
-    name: 'UserProfile',
-    component: UserProfile,
-    meta: { requiresAuth: true }
-  },
-  // Define other routes here
 ];
 
 const router = createRouter({
