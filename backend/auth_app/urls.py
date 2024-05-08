2460334609c0406203d382d09839606f9e344217
Jonas Mørkland <jonasmorkland@outlook.com>
2024-05-08 13:01:10 +0200

    Added check such that when valid cookie is present on portal, portal automatically daisplays authentication message.

--- backend/auth_app/urls.py
+++ backend/auth_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import LoginView
 from .views import LogoutView
+from .views import VerifyView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify/', VerifyView.as_view(), name='verify')
 ]
